246c4b66e3382c00fa6e8b28755c31b82fa951ed
lukeabarry <lukeabarry@gmail.com>
2025-10-23 14:27:13 -0400

    added a few files

--- MIDI_interpretation_module.py
+++ MIDI_interpretation_module.py
@@ -1,5 +1,20 @@
 from mido import MidiFile
-midifilepath = 'MIDIFiles/scottish_1_(c)taylor.mid'
+import time
+midifilepath = 'MIDIFiles/devkitchenP.mid'
 midi_data = MidiFile(midifilepath)
-track = enumerate(midi_data.tracks)
+track = midi_data.tracks[1]  # Accessing the second track (index 1)
 
+print('Track {}: {}'.format(1, track.name))
+for msg in track:
+    if msg.type == 'set_tempo':
+        print(f"Set Tempo: {msg.tempo} Time: {msg.time}")
+    elif msg.type == 'note_on':
+        print(f"Note On: Note {msg.note} Velocity {msg.velocity} Time: {msg.time}")
+        wait_time = msg.time
+        wait_time_seconds = wait_time / 1000.0  # Convert milliseconds to seconds
+        time.sleep(wait_time_seconds)
+    elif msg.type == 'note_off':
+        print(f"Note Off: Note {msg.note} Velocity {msg.velocity} Time: {msg.time}")
+        wait_time = msg.time
+        wait_time_seconds = wait_time / 1000.0  # Convert milliseconds to seconds
+        time.sleep(wait_time_seconds)
